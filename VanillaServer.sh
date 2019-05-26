66f93b6f65e5bb8935fea59a38af209455847fc3
Joseph <salomj9@gmail.com>
2019-05-26 13:00:30 -0700

    Update VanillaServer.sh

--- VanillaServer.sh
+++ VanillaServer.sh
@@ -5,19 +5,23 @@ minecraft_jar='minecraft-server-1.14.1.jar'
 input_file="${minecraft_dir}/logs/input.txt"
 output_file="${minecraft_dir}/logs/output.log"
 
-start_script="java -server -Xmx16384M -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=5 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar $minecraft_jar nogui"
+start_script="java -server -Xmx16384M -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=5 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar '$minecraft_jar' nogui"
+
+clean() {
+	rm -f "$input_file"
+	rm -f "$output_file"
+	rm -rf "${minecraft_dir}/logs"
+}
 
 start() {
-	stop
+	mkdir -p $(dirname "$input_file")
+	mkdir -p $(dirname "$output_file")
+	rm -f "$input_file"
+	rm -f "$output_file"
+	touch "$input_file"
+	touch "$output_file"
 	
-	rm -rf $input_file
-	rm -rf $output_file
-	touch $input_file
-	touch $output_file
-	
-	nohup sh -c "cd $minecraft_dir; tail -f $input_file | $start_script" >> $output_file &
-
-	connect
+	nohup sh -c "cd $minecraft_dir; tail -f $input_file | $start_script" >> "$output_file" &
 }
 
 connect() {
@@ -31,7 +35,7 @@ output() {
 
 input() {
 	while IFS= read -r line; do
-  		echo "$line" >> $input_file
+  		echo "$line" >> "$input_file"
 	done
 }
 
@@ -39,7 +43,7 @@ stop() {
 	local process="$(getProcess)"
 	if [ ! -z "$process" ]; then
 		echo "Sending 'stop' to $input_file"
-		echo 'stop' >> $input_file
+		echo 'stop' >> "$input_file"
 		sleep 10
 	fi
 	
@@ -64,19 +68,24 @@ stop() {
 }
 
 getProcess() {
-	echo "$(ps aux | grep "[j]ava.*-jar $minecraft_jar" | awk '{print $2}')"
+	echo "$(ps aux | grep "[j]ava.*-jar '$minecraft_jar'" | awk '{print $2}')"
 }
 
 if [ "$1" == 'start' ]; then
+	stop
 	start
+	connect
 elif [ "$1" == 'connect' ]; then
 	connect
 elif [ "$1" == 'input' ]; then
 	input
 elif [ "$1" == 'output' ]; then
 	output
+elif [ "$1" == 'clean' ]; then
+	stop
+	clean
 elif [ "$1" == 'stop' ]; then
 	stop
 else
-	echo "Usage: $0 [start|connect|input|output|stop]"
+	echo "Usage: $0 [start|connect|input|output|clean|stop]"
 fi
