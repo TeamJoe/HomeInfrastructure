71de5da18aed8224323913a384f66ae67d0de876
Joseph <salomj9@gmail.com>
2019-06-02 09:53:41 -0700

    Update VanillaServer.sh

--- VanillaServer.sh
+++ VanillaServer.sh
@@ -5,7 +5,8 @@ minecraft_jar='minecraft-server-1.14.1.jar'
 input_file="${minecraft_dir}/logs/input.txt"
 output_file="${minecraft_dir}/logs/output.log"
 
-start_script="java -server -Xmx16384M -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=5 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar '$minecraft_jar' nogui"
+//start_script="java -server -Xmx16G -Xms16G -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=5 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar '$minecraft_jar' nogui"
+start_script="java -server -Xms16G -Xmx16G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=35 -XX:G1MaxNewSizePercent=60 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AlwaysPreTouch -jar '$minecraft_jar' nogui"
 
 clean() {
 	rm -f "$input_file"
@@ -20,7 +21,7 @@ start() {
 	rm -f "$output_file"
 	touch "$input_file"
 	touch "$output_file"
-	nohup sh -c "cd $minecraft_dir; tail -f -n 0 $input_file | $start_script" >> "$output_file" &
+	nohup sh -c "cd $minecraft_dir; tail -f -n 0 $input_file | $start_script &" >> "$output_file" &
 }
 
 connect() {
