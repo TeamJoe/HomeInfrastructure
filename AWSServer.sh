eb9ce25767db032169dd7a9bbe549d02f8d664b4
Joseph <salomj9@gmail.com>
2019-06-07 13:50:34 -0700

    Update AWSServer.sh

--- AWSServer.sh
+++ AWSServer.sh
@@ -7,7 +7,7 @@ output_file="${minecraft_dir}/logs/output.log"
 
 #start_script="java -server -Xmx16G -Xms16G -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=5 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar '$minecraft_jar' nogui"
 #start_script="java -server -Xms16G -Xmx16G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=35 -XX:G1MaxNewSizePercent=60 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AlwaysPreTouch -jar '$minecraft_jar' nogui"
-start_script="java -server -Xmx868M -Xms868M -jar '$minecraft_jar' nogui"
+start_script="java -server -Xmx956M -Xms956M -jar '$minecraft_jar' nogui"
 
 clean() {
 	killProcess "$(ps aux | grep '[t]ail' | awk '{print $2}')"
