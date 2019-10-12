1eda28123d3cc971e49ebb95d8d39bab192c0c71
Joseph <salomj9@gmail.com>
2019-10-12 09:52:28 -0700

    Create domain.sh

---
+++ domain.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+IP_FILE_LOCATION='/root/ip.txt'
+LOG_LOCATION='/root/ip-logs.txt'
+
+updateDomains() {
+	#curl <REDACTED:http://freedns.afraid.org/dynamic/> >> "${LOG_LOCATION}"
+}
+
+getCurrentIP() {
+	echo "$(curl ifconfig.co)"
+}
+
+getLastIP() {
+	echo "$(cat ""${IP_FILE_LOCATION}"")"
+}
+
+check() {
+	local currentIP="$(getCurrentIP)"
+	local lastIP="$(getLastIP)"
+	
+	if [ "${currentIP}" != "${lastIP}" ]; then
+		local now="$(date +"%T")"
+		echo "${now}: ${currentIP} != ${lastIP}" >> "${LOG_LOCATION}"
+		updateDomains
+		echo "${currentIP}" > "${IP_FILE_LOCATION}"
+	fi
+}
+
+check
