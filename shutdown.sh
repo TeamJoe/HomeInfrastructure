38bd0dc46675b9fbbd34ece6fbffc9838d7190ba
Joseph <salomj9@gmail.com>
2019-10-26 18:18:54 -0700

    Create shutdown.sh

---
+++ shutdown.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+command="/home/joe/ATM3Server.sh active"
+minimum_server_boot_time=3600
+
+checkActive() {
+	local timeSinceBoot="$(printf '%.0f\n' "$(awk '{print $1}' /proc/uptime)")"
+	
+	if [ $minimum_server_boot_time -lt $timeSinceBoot ]; then
+		local isActive="$($command)"
+		if [ "$isActive" == "false" ]; then
+			shutdown
+		fi
+	fi
+}
+
+checkActive
