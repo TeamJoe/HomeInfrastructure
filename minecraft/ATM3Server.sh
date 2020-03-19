14b18773ba4965d2c01b7dd6b993cdd8674c25fc
Joseph <salomj9@gmail.com>
2020-03-18 22:21:43 -0700

    Update ATM3Server.sh

--- minecraft/ATM3Server.sh
+++ minecraft/ATM3Server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 path="$0"
-minecraft_dir='/home/minecraft/data/ATM3/1.5.1'
-minecraft_jar='forge-1.12.2-14.23.5.2844-universal.jar'
-#minecraft_dir='/home/minecraft/data/ATM3/1.5.4'
-#minecraft_jar='forge-1.12.2-14.23.5.2847-universal.jar'
+#minecraft_dir='/home/minecraft/data/ATM3/1.5.1'
+#minecraft_jar='forge-1.12.2-14.23.5.2844-universal.jar'
+minecraft_dir='/home/minecraft/data/ATM3/1.5.4'
+minecraft_jar='forge-1.12.2-14.23.5.2847-universal.jar'
 start_script="java -Xms32G -Xmx32G -d64 -server -XX:+AggressiveOpts -XX:ParallelGCThreads=3 -XX:+UseConcMarkSweepGC -XX:+UnlockExperimentalVMOptions -XX:+UseParNewGC -XX:+ExplicitGCInvokesConcurrent -XX:MaxGCPauseMillis=10 -XX:GCPauseIntervalMillis=50 -XX:+UseFastAccessorMethods -XX:+OptimizeStringConcat -XX:NewSize=84m -XX:+UseAdaptiveGCBoundary -XX:NewRatio=3 -Dfml.readTimeout=90 -Dfml.queryResult=confirm -jar \"${minecraft_jar}\" nogui"
 
 /home/minecraft/GenericMinecraftServer-1-12.2.sh "$path" "$minecraft_dir" "$minecraft_jar" "$start_script" "$@"
