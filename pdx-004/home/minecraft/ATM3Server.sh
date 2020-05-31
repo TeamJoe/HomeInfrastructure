f82fe34acf1a4d3418cfabf0a03aa95b0bc3ca79
JoJo <joseph.salomone@dealertrack.com>
2020-05-30 23:48:20 -0700

    Add Mohist due to lag issues

--- pdx-004/home/minecraft/ATM3Server.sh
+++ pdx-004/home/minecraft/ATM3Server.sh
@@ -5,7 +5,11 @@ external_address="lobythepirate.mooo.com"
 #minecraft_dir='/home/minecraft/data/ATM3/1.5.1'
 #minecraft_jar='forge-1.12.2-14.23.5.2844-universal.jar'
 minecraft_dir='/home/minecraft/data/ATM3/1.5.4'
-minecraft_jar='forge-1.12.2-14.23.5.2847-universal.jar'
-start_script="java -Xms32G -Xmx32G -d64 -server -XX:+AggressiveOpts -XX:ParallelGCThreads=3 -XX:+UseConcMarkSweepGC -XX:+UnlockExperimentalVMOptions -XX:+UseParNewGC -XX:+ExplicitGCInvokesConcurrent -XX:MaxGCPauseMillis=10 -XX:GCPauseIntervalMillis=50 -XX:+UseFastAccessorMethods -XX:+OptimizeStringConcat -XX:NewSize=84m -XX:+UseAdaptiveGCBoundary -XX:NewRatio=3 -Dfml.readTimeout=90 -Dfml.queryResult=confirm -jar \"${minecraft_jar}\" nogui"
+#minecraft_jar='forge-1.12.2-14.23.5.2847-universal.jar'
+#start_script="java -Xms32G -Xmx32G -d64 -server -XX:+AggressiveOpts -XX:ParallelGCThreads=3 -XX:+UseConcMarkSweepGC -XX:+UnlockExperimentalVMOptions -XX:+UseParNewGC -XX:+ExplicitGCInvokesConcurrent -XX:MaxGCPauseMillis=10 -XX:GCPauseIntervalMillis=50 -XX:+UseFastAccessorMethods -XX:+OptimizeStringConcat -XX:NewSize=84m -XX:+UseAdaptiveGCBoundary -XX:NewRatio=3 -Dfml.readTimeout=90 -Dfml.queryResult=confirm -jar \"${minecraft_jar}\" nogui"
+#minecraft_jar='Magma-3d116a0-server.jar'
+minecraft_jar='Mohist-1.12.2-1010498-server.jar'
+start_script="java -Xms32G -Xmx32G -d64 -server -jar \"${minecraft_jar}\" nogui"
+
 
 /home/minecraft/GenericMinecraftServer-1-12.2.sh "$path" "$external_address" "$minecraft_dir" "$minecraft_jar" "$start_script" "$@"
