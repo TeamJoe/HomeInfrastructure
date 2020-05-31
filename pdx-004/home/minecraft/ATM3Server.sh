9b15bc3dc7a62a4bb53b31bfd9a9144cd1357240
JoJo <joseph.salomone@dealertrack.com>
2020-05-31 12:46:20 -0700

    server settings

--- pdx-004/home/minecraft/ATM3Server.sh
+++ pdx-004/home/minecraft/ATM3Server.sh
@@ -9,6 +9,7 @@ minecraft_dir='/home/minecraft/data/ATM3/1.5.4'
 #start_script="java -Xms32G -Xmx32G -d64 -server -XX:+AggressiveOpts -XX:ParallelGCThreads=3 -XX:+UseConcMarkSweepGC -XX:+UnlockExperimentalVMOptions -XX:+UseParNewGC -XX:+ExplicitGCInvokesConcurrent -XX:MaxGCPauseMillis=10 -XX:GCPauseIntervalMillis=50 -XX:+UseFastAccessorMethods -XX:+OptimizeStringConcat -XX:NewSize=84m -XX:+UseAdaptiveGCBoundary -XX:NewRatio=3 -Dfml.readTimeout=90 -Dfml.queryResult=confirm -jar \"${minecraft_jar}\" nogui"
 #minecraft_jar='Magma-3d116a0-server.jar'
 minecraft_jar='Mohist-1.12.2-1010498-server.jar'
-start_script="java -Xms32G -Xmx32G -d64 -server -Dlog4j.configurationFile=log4j2.xml -jar \"${minecraft_jar}\" nogui"
+#start_script="java -Xms32G -Xmx32G -d64 -server -Dlog4j.configurationFile=log4j2.xml -jar \"${minecraft_jar}\" nogui"
+start_script="java -Xms32G -Xmx32G -d64 -server -d64 -server -XX:+AggressiveOpts -XX:ParallelGCThreads=3 -XX:+UseConcMarkSweepGC -XX:+UnlockExperimentalVMOptions -XX:+UseParNewGC -XX:+ExplicitGCInvokesConcurrent -XX:MaxGCPauseMillis=10 -XX:GCPauseIntervalMillis=50 -XX:+UseFastAccessorMethods -XX:+OptimizeStringConcat -XX:NewSize=84m -XX:+UseAdaptiveGCBoundary -XX:NewRatio=3 -Dfml.readTimeout=90 -Dfml.queryResult=confirm -Dlog4j.configurationFile=log4j2.xml -jar \"${minecraft_jar}\" nogui"
 
 /home/minecraft/GenericMohistServer-1-12.2.sh "$path" "$external_address" "$minecraft_dir" "$minecraft_jar" "$start_script" "$@"
