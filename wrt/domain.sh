c6523deb4b39deb25a8e0bcca74f13f93dd11f34
JoJo <joseph.salomone@dealertrack.com>
2020-06-24 12:47:54 -0700

    Handle IPv4 and IPv6, also checks multiple domains for IP address

--- wrt/domain.sh
+++ wrt/domain.sh
@@ -2,31 +2,64 @@
 # /root/domain.sh
 # /etc/crontabs/root: "*/5 * * * * /root/domain.sh"
 
-IP_FILE_LOCATION='/root/ip.txt'
+IPV4_FILE_LOCATION='/root/ipv4.txt'
+IPV6_FILE_LOCATION='/root/ipv6.txt'
 LOG_LOCATION='/root/ip-logs.txt'
 
-updateDomains() {
-	#curl <REDACTED:http://freedns.afraid.org/dynamic/> >> "${LOG_LOCATION}"
+updateDomainsIP() {
+	#curl -s -f $@ <REDACTED:http://freedns.afraid.org/dynamic/> >> "${LOG_LOCATION}" 2>&1
 }
 
-getCurrentIP() {
-	echo "$(curl ifconfig.co)"
-}
-
-getLastIP() {
-	echo "$(cat ""${IP_FILE_LOCATION}"")"
-}
-
-check() {
-	local currentIP="$(getCurrentIP)"
-	local lastIP="$(getLastIP)"
-	
-	if [ "${currentIP}" != "${lastIP}" ]; then
-		local now="$(date +"%T")"
-		echo "${now}: ${currentIP} != ${lastIP}" >> "${LOG_LOCATION}"
-		updateDomains
-		echo "${currentIP}" > "${IP_FILE_LOCATION}"
+getCurrenIPFromDomain() {
+	local IP="$(curl -s -f $@)"
+	if [ -n "$(echo "$IP" | grep -i 'error')" ]; then
+		echo ""
+	else
+		echo "$IP"
 	fi
 }
 
-check
+getCurrentIP() {
+	local IP="$(getCurrenIPFromDomain $@ ifconfig.me)"
+	if [ -z "$IP" ]; then
+		local IP="$(getCurrenIPFromDomain $@ ifconfig.co)"
+	fi
+
+	if [ -n "$IP" ]; then
+		echo "$IP"
+	else
+		echo "NO IP FOUND"
+	fi
+}
+
+getIPSaveLocation() {
+	if [ "$1" == "-4" ]; then
+		echo "${IPV4_FILE_LOCATION}"
+	elif [ "$1" == "-6" ]; then
+		echo "${IPV6_FILE_LOCATION}"
+	fi
+}
+
+getLastIP() {
+	echo "$(cat "$(getIPSaveLocation $@)" | echo "NO PREVIOUS IP")"
+}
+
+checkIP() {
+	touch -a "$(getIPSaveLocation $@)"
+
+	local currentIP="$(getCurrentIP $@)"
+	local lastIP="$(getLastIP $@)"
+
+	if [ "${currentIP}" != "${lastIP}" ]; then
+		local now="$(date +"%T")"
+		echo "${now}: ${currentIP} != ${lastIP}" >> "${LOG_LOCATION}"
+		updateDomainsIP $@
+		if [ "${currentIP}" != "NO IP FOUND" ]; then
+			echo "${currentIP}" > "$(getIPSaveLocation $@)"
+		fi
+	fi
+}
+
+checkIP -4
+checkIP -6
+
