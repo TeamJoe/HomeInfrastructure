536276aa6e406d0a8ecf2b94f986c805032fb011
JoJo <joseph.salomone@dealertrack.com>
2022-09-23 03:05:38 -0700

    Utilize new logs

--- servers/pdx-003/home/satisfactory/GenericSatisfactoryServer.sh
+++ servers/pdx-003/home/satisfactory/GenericSatisfactoryServer.sh
@@ -21,12 +21,13 @@ if [ -n "${prefix}" ]; then
   installDirectory="$(getProperty "${prefix}.dir.install")"
 fi
 
-simple_output_file="${installDirectory}/logs/output.log"
-simple_server_start_regex='.*Booting Server'
-#server_start_regex='Took ([0-9]+\.[0-9]+) seconds to LoadMap'
-server_start_regex='\[([^]]+)\].*Created socket for bind address'
-player_join_regex='Join\ssucceeded:\s(.+)'
-player_leave_regex='UNetConnection::Close.*Driver:\s+GameNetDriver.*UniqueId:\s+([^,]+),'
+simple_log_file="${installDirectory}/logs/simple.log"
+extended_log_file="${installDirectory}/logs/FactoryGame.log"
+player_list_file="${installDirectory}/logs/user.csv"
+simple_server_date_pattern='\[([^]]+)\].*'
+server_start_regex='\[([^]]+)\].*Server Started.*'
+player_join_regex='\[([^]]+)\].*Player Joined.*\((.*)\)'
+player_leave_regex='\[([^]]+)\].*Player Left.*\((.*)\)'
 
 externalAddress="${address}:${queryport}"
 startParameters=$(echo \
@@ -54,34 +55,11 @@ startParameters=$(echo \
 #++++++++++++++++++++
 
 getSimpleLogFile() {
-	echo "${installDirectory}/logs/simple.log"
-}
-
-getLogFile() {
-	echo "${installDirectory}/logs/FactoryGame.log"
-}
-
-extractLogValue() {
-	local regex_pattern="$1"; shift
-	local extract_group="$1"; shift
-	local default_value="$1"; shift
-	local get_last="$1"; shift
-
-	local match=""
-	local output="$default_value"
-	local line=""
-	IFS=$'\n'
-	
-	for match in $(cat "$(getLogFile)" | regexExtract "$regex_pattern" $extract_group); do
-		if [[ -n "$match" ]]; then
-			output="$match"
-			if [[ "$get_last" != "true" ]]; then
-				break;
-			fi
-		fi
-	done
-	
-	echo "$output"
+  if [[ -f "${simple_log_file}" ]]; then
+    echo "${simple_log_file}"
+  else
+	  echo "$(ls -Art "${installDirectory}/logs/"simple* | tail --lines=1)"
+  fi
 }
 
 processDate()
@@ -95,8 +73,7 @@ processDate()
 	rawDate="$( echo "${rawDate//[-:\. ]/ }" | tr ' ' '\n' )"
 	
 	IFS=$'\n'
-	for date in $rawDate
-	do
+	for date in ${rawDate}; do
 		if [[ "$count" -lt 2 ]]; then
 			output="${output}${date}-"
 		elif [[ "$count" -eq 2 ]]; then
@@ -120,16 +97,12 @@ processDate()
 
 getBootTime() {
 	local fileNameMatcher='simple\.([^\.]+)\.log'
-	local rawDate=''
-	
-	rawDate="$(head --lines=1 "$(getSimpleLogFile)" | regexExtract "$fileNameMatcher" 1)"
+	local rawDate="$(head --lines=1 "${simple_log_file}" | regexExtract "$fileNameMatcher" 1)"
 	echo "$(processDate "$rawDate")"
 }
 
 getStartTime() {
-	local rawDate=''
-	
-	rawDate="$(extractLogValue "$server_start_regex" 1 "" "false")"
+	local rawDate="$(cat "${simple_log_file}" | regexExtract "${server_start_regex}" 1 | trim | tail --lines=1)"
 	echo "$(processDate "$rawDate")"
 }
 
@@ -138,32 +111,17 @@ getTimeSinceServerBoot() {
 }
 
 getLastActivityTime() {
-	local simple_server_date_pattern='\[([^\]]+)\].*'
+  local rawDate=''
+  local log_file="$(getSimpleLogFile)"
 
-	local rawDate=''
-	local count=0
-	local date=''
-	local output=''
-
-	local match=""
-	local output=""
-	local line=""
-	IFS=$'\n'
-	
-	for line in $(tail --lines=25 "$(getLogFile)"); do
-		match="$(regexExtract "$line" "$simple_server_date_pattern" 1)"
-		if [ -n "$match" ]; then
-			output="$match"
-		fi
-	done
-	
-	if [ -n "$output" ]; then
-		echo "$(processDate "$output")"
+	local rawDate="$(cat "${log_file}" | regexExtract "${server_start_regex}" 1 | trim | tail --lines=1)"
+	if [[ -n "${rawDate}" ]]; then
+		echo "$(processDate "${rawDate}")"
 	fi
 }
 
 getUptime() {
-	if [ "$(isStarted)" != "true" ]; then
+	if [[ "$(isStarted)" != "true" ]]; then
 		echo "0"
 	else
 		local currentTimeStamp="$(date +"%s")"
@@ -179,7 +137,7 @@ getUptime() {
 #++++++++++++++++++++
 
 isBooted() {
-	if [ "$(statusService "${service}")" != "Powered On" ]; then
+	if [[ "$(statusService "${service}")" != "Powered On" ]]; then
 		echo "false"
 	else
 		echo "true"
@@ -193,7 +151,7 @@ isStarted() {
 		local bootTimeStamp="$(getBootTime)"
 		local startTimeStamp="$(getStartTime)"
 		
-		if [ $startTimeStamp -ge $bootTimeStamp ]; then
+		if [[ $startTimeStamp -ge $bootTimeStamp ]]; then
 			echo "true"
 		else
 			echo "false"
@@ -202,12 +160,10 @@ isStarted() {
 }
 
 getPlayerCount() {
-	if [ "$(isStarted)" != "true" ]; then
+	if [[ "$(isStarted)" != "true" || "$(cat "${player_list_file}" | wc --lines)" == '0' ]]; then
 		echo "0"
 	else
-		local joined="$(cat "$(getLogFile)" | regexCount "$player_join_regex")"
-		local left="$(cat "$(getLogFile)" | regexCount "$player_leave_regex")"
-		echo "$(($joined - $left))"
+		echo "$(cat "${player_list_file}" | wc --lines) ($(cat "${player_list_file}" | regexExtract '\s+(.*)' 1 | regexReplaceMultiline '\s+' ' ' | trim))"
 	fi
 }
 
@@ -221,12 +177,12 @@ isServerActive() {
 	local minimumActiveRemaining=''
 
 	currentTimeStamp="$(date +"%s")"
-	if [ "$(isStarted)" != "true" ]; then
-		if [ "$(isBooted)" == "true" ]; then
+	if [[ "$(isStarted)" != "true" ]]; then
+		if [[ "$(isBooted)" == "true" ]]; then
 			startTimeStamp="$(getBootTime)"
 			timeSinceStart="$((currentTimeStamp-startTimeStamp))"
 			minimumBootRemaining="$((minimum_server_boot_time-timeSinceStart))"
-			if [ "$minimumBootRemaining" -ge "0" ]; then
+			if [[ "$minimumBootRemaining" -ge "0" ]]; then
 				echo "$minimumBootRemaining"
 			else
 				echo "0"
@@ -239,8 +195,8 @@ isServerActive() {
 		timeSinceStart="$((currentTimeStamp-startTimeStamp))"
 		minimumBootRemaining="$((minimum_server_boot_time-timeSinceStart))"
 		
-		if [ ! "$(getPlayerCount)" == 0 ]; then
-			if [ "$minimumBootRemaining" -ge "$minimum_disconnect_live_time" ]; then
+		if [[ ! "$(getPlayerCount)" == '0' ]]; then
+			if [[ "$minimumBootRemaining" -ge "$minimum_disconnect_live_time" ]]; then
 				echo "$minimumBootRemaining"
 			else
 				echo "$minimum_disconnect_live_time"
@@ -249,9 +205,9 @@ isServerActive() {
 			lastActivityTimeStamp="$(getLastActivityTime)"
 			timeSinceActive="$((currentTimeStamp-lastActivityTimeStamp))"
 			minimumActiveRemaining="$((minimum_disconnect_live_time-timeSinceActive))"
-			if [ "$minimumBootRemaining" -ge 0 ] && [ "$minimumBootRemaining" -ge "$minimumActiveRemaining" ]; then
+			if [[ "$minimumBootRemaining" -ge 0 && "$minimumBootRemaining" -ge "$minimumActiveRemaining" ]]; then
 				echo "$minimumBootRemaining"
-			elif [ "$minimumActiveRemaining" -ge 0 ]; then
+			elif [[ "$minimumActiveRemaining" -ge 0 ]]; then
 				echo "$minimumActiveRemaining"
 			else
 				echo "0"
@@ -315,40 +271,31 @@ startServer() {
 	startService "${service}" ${startParameters[@]}
 }
 
-log() {
-	echo "[$(date +"%D %T")] ${1}" >> "${simple_output_file}"
-	sendMessage "${description}: ${1}"
-}
-
 monitorLogs() {
-  sleep 10
-
   while [[ "$(statusService "${service}")" == "Powered On" ]];  do
-    tail --follow --lines=1000 "$(getLogFile)" | processLog
-    sleep 5
+    tail --follow=name --lines 0 "${simple_log_file}" | while read line; do
+      processLog "${line}"
+      sleep 5
+    done
   done
-
-  log "Server Stopped"
 }
 
 processLog() {
   local match=""
 
-  match="$(cat - | regexExtract "${server_start_regex}" 1)"
+  match="$(regexExtract "${1}" "${server_start_regex}" 1)"
   if [[ -n "${match}" ]]; then
-    log "Server Started (${match})"
+    sendMessage "Server Started"
   fi
 
-  match="$(cat - | regexExtract regexExtract "${player_join_regex}" 1)"
+  match="$(regexExtract "${1}" "${player_join_regex}" 2)"
   if [[ -n "${match}" ]]; then
-    log "Player Joined (${match})"
-    log "Player Count $(getPlayerCount)"
+    sendMessage "Player Joined (${match})"
   fi
 
-  match="$(cat - | regexExtract regexExtract "${player_leave_regex}" 1)"
+  match="$(regexExtract "${1}" "${player_leave_regex}" 2)"
   if [[ -n "${match}" ]]; then
-    log "Player Left (${match})"
-    log "Player Count $(getPlayerCount)"
+    sendMessage "Player Left (${match})"
   fi
 }
 
@@ -359,7 +306,7 @@ runCommand() {
 	if [[ "${command}" == "info" ]]; then
 		echo "Address: ${address} Port: ${queryport}"
 	elif [[ "${command}" == "logs" ]]; then
-		tail --lines=1000 "$(getLogFile)"
+		tail --lines=1000 "${extended_log_file}"
 	elif [[ "${command}" == "simple" ]]; then
 		tail --lines=1000 "$(getSimpleLogFile)"
 	elif [[ "${command}" == "status" ]]; then
@@ -381,8 +328,8 @@ runCommand() {
 	elif [[ "${command}" == "command" ]]; then
 		sendCommand "${service}" ${@}
 	elif [[ "${command}" == "start-monitor" ]]; then
-		startServer
 		monitorLogs &
+		startServer
 		monitorService "${service}"
 	elif [[ "${command}" == "monitor" ]]; then
 		monitorService "${service}"
@@ -396,8 +343,7 @@ runCommand() {
 		echo "${externalAddress}"
 	elif [[ "${command}" == "stop" ]]; then
 		stopService "${service}"
-		killProcess $(getProcess 'tail' "$(getLogFile)")
-		log "Server Stopped"
+		killProcess $(getProcess 'tail' "${simple_log_file}")
 	else
 		echo "Usage: $runPath [start|start-monitor|debug|monitor|command|uptime|booted|started|active|list|status|ip|bash|info|logs|simple|description|address|stop]"
 		exit 1
--- servers/pdx-003/server/regex.sh
+++ servers/pdx-003/server/regex.sh
@@ -4,27 +4,27 @@
 # Usage: | regex [Expression]
 # Usage: regex [Value] [Expression]
 regex() {
-  if [[ -p /dev/stdin ]]; then
-    cat - | sed --regexp-extended "${1}"
+  if [[ -z "${2}" && -p /dev/stdin ]]; then
+    cat - | sed --regexp-extended --unbuffered "${1}"
   else
-    echo "${1}" | sed --regexp-extended "${2}"
+    echo "${1}" | sed --regexp-extended --unbuffered "${2}"
   fi
 }
 
 # Usage: | regexMultiline [Expression]
 # Usage: regexMultiline [Value] [Expression]
 regexMultiline() {
-  if [[ -p /dev/stdin ]]; then
-    cat - | tr --delete '\r' | tr '\n' '\r' | sed --regexp-extended "${1}" | tr '\r' '\n'
+  if [[ -z "${2}" && -p /dev/stdin ]]; then
+    cat - | tr --delete '\r' | tr '\n' '\r' | sed --regexp-extended --unbuffered "${1}" | tr '\r' '\n'
   else
-    echo "${1}" | tr --delete '\r' | tr '\n' '\r' | sed --regexp-extended "${2}" | tr '\r' '\n'
+    echo "${1}" | tr --delete '\r' | tr '\n' '\r' | sed --regexp-extended --unbuffered "${2}" | tr '\r' '\n'
   fi
 }
 
 # Usage: | regexReplace [Pattern] [Replacement]
 # Usage: regexReplace [Value] [Pattern] [Replacement]
 regexReplace() {
-  if [[ -p /dev/stdin ]]; then
+  if [[ -z "${3}" && -p /dev/stdin ]]; then
     cat - | regex "s/${1//\//\\/}/${2//\//\\/}/g"
   else
     regex "${1}" "s/${2//\//\\/}/${3//\//\\/}/g"
@@ -34,7 +34,7 @@ regexReplace() {
 # Usage: | regexReplaceMultiline [Pattern] [Replacement]
 # Usage: regexReplaceMultiline [Value] [Pattern] [Replacement]
 regexReplaceMultiline() {
-  if [[ -p /dev/stdin ]]; then
+  if [[ -z "${3}" && -p /dev/stdin ]]; then
     cat - | regexMultiline "s/${1//\//\\/}/${2//\//\\/}/g"
   else
     regexMultiline "${1}" "s/${2//\//\\/}/${3//\//\\/}/g"
@@ -44,27 +44,27 @@ regexReplaceMultiline() {
 # Usage: | regexFind [Pattern]
 # Usage: regexFind [Value] [Pattern]
 regexFind() {
-  if [[ -p /dev/stdin ]]; then
-    cat - | grep --only-matching --extended-regexp "${1}"
+  if [[ -z "${2}" && -p /dev/stdin ]]; then
+    cat - | grep --only-matching --extended-regexp --line-buffered "${1}"
   else
-    echo "${1}" | grep --only-matching --extended-regexp "${2}"
+    echo "${1}" | grep --only-matching --extended-regexp --line-buffered "${2}"
   fi
 }
 
 # Usage: | regexFindMultiline [Pattern]
 # Usage: regexFindMultiline [Value] [Pattern]
 regexFindMultiline() {
-  if [[ -p /dev/stdin ]]; then
-    cat - | tr --delete '\r' | tr '\n' '\r' | grep --only-matching --extended-regexp "${1}" | tr '\r' '\n'
+  if [[ -z "${2}" && -p /dev/stdin ]]; then
+    cat - | tr --delete '\r' | tr '\n' '\r' | grep --only-matching --extended-regexp --line-buffered "${1}" | tr '\r' '\n'
   else
-    echo "${1}" | tr --delete '\r' | tr '\n' '\r' | grep --only-matching --extended-regexp "${2}" | tr '\r' '\n'
+    echo "${1}" | tr --delete '\r' | tr '\n' '\r' | grep --only-matching --extended-regexp --line-buffered "${2}" | tr '\r' '\n'
   fi
 }
 
 # Usage: | regexExtract [Pattern] [Group (Defaults: 0)]
 # Usage: regexExtract [Value] [Pattern] [Group (Defaults: 0)]
 regexExtract() {
-  if [[ -p /dev/stdin ]]; then
+  if [[ -z "${3}" && -p /dev/stdin ]]; then
     cat - | regexFind "${1}" | regexReplace "${1}" "\\${2:-0}"
   else
     regexFind "${1}" "${2}" | regexReplace "${2}" "\\${3:-0}"
@@ -74,7 +74,7 @@ regexExtract() {
 # Usage: | regexExtractMultiline [Pattern] [Group (Defaults: 0)]
 # Usage: regexExtractMultiline [Value] [Pattern] [Group (Defaults: 0)]
 regexExtractMultiline() {
-  if [[ -p /dev/stdin ]]; then
+  if [[ -z "${3}" && -p /dev/stdin ]]; then
     cat - | tr --delete '\r' | tr '\n' '\r' | regexFind "${1}" | regexReplace "${1}" "\\${2:-0}" | tr '\r' '\n'
   else
     echo "${1}" | tr --delete '\r' | tr '\n' '\r' | regexFind "${2}" | regexReplace "${2}" "\\${3:-0}" | tr '\r' '\n'
@@ -85,7 +85,7 @@ regexExtractMultiline() {
 # Usage: regexCount [Value] [Pattern]
 regexCount() {
   local value=""
-  if [[ -p /dev/stdin ]]; then
+  if [[ -z "${2}" && -p /dev/stdin ]]; then
     value="$(cat - | regexFind "${1}" | regexReplace "${1}" "1")"
   else
     value="$(regexFind "${1}" "${2}" | regexReplace "${2}" "1")"
@@ -102,7 +102,7 @@ regexCount() {
 # Usage: regexCountMultiline [Value] [Pattern]
 regexCountMultiline() {
   local value=""
-  if [[ -p /dev/stdin ]]; then
+  if [[ -z "${2}" && -p /dev/stdin ]]; then
     value="$(cat - | tr --delete '\r' | tr '\n' '\r' | regexFind "${1}" | regexReplace "${1}" "1" | tr '\r' '\n')"
   else
     value="$(echo "${1}" | tr --delete '\r' | tr '\n' '\r' | regexFind "${2}" | regexReplace "${2}" "1" | tr '\r' '\n')"
@@ -118,7 +118,7 @@ regexCountMultiline() {
 # Usage: | trim [Trim-Pattern (Defaults: \s)]
 # Usage: trim [Value] [Trim-Pattern (Defaults: \s)]
 trim() {
-  if [[ -p /dev/stdin ]]; then
+  if [[ -z "${2}" && -p /dev/stdin ]]; then
     cat - | regexReplaceMultiline "(^${1:-\s}+|${1:-\s}+$)" ""
   else
     regexReplaceMultiline "${1}" "(^${2:-\s}+|${2:-\s}+$)" ""
