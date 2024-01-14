a211f561cc74726157b7b63974cafed0a6a76f2e
JoJo <salomj9@gmail.com>
2024-01-14 00:44:21 -0800

    Added the ability to run the metadata in single shot mode

--- servers/DaveyJonesLocker/home/plexmeta/plexmeta.sh
+++ servers/DaveyJonesLocker/home/plexmeta/plexmeta.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
 # /home/plexmeta/plexmeta.sh
 
-service='metadata'
-description='Plex Meta Manager'
-timezone="$(/server/Properties.sh 'timezone')"
-architecture="$(/server/Properties.sh 'architecture')"
-startParameters=$(echo \
-                "--env PUID=$(id -u plexmeta)" \
-                "--env PGID=$(id -g plexmeta)" \
-                "--env TZ=${timezone}" \
-                "--env VERSION=latest" \
-                "--env PMM_CONFIG=/config/config.yml" \
-                "--env PMM_TIME=03:00" \
-                "--env PMM_RUN=False" \
-                "--env PMM_TEST=False" \
-                "--env PMM_NO_MISSING=False" \
-                "--mount type=bind,source=/home/plexmeta,target=/config" \
-                "--mount type=bind,source=/home/plexmeta,target=/home/plexmeta" \
-                "--restart unless-stopped lscr.io/linuxserver/plex-meta-manager:${architecture}-latest" \
-                )
+
+if [[ "$2" == 'once' ]]; then
+  service='oncemeta'
+  description='Plex Meta Manager [Single Shot]'
+  timezone="$(/server/Properties.sh 'timezone')"
+  architecture="$(/server/Properties.sh 'architecture')"
+  startParameters=$(echo \
+                  "--env PUID=$(id -u plexmeta)" \
+                  "--env PGID=$(id -g plexmeta)" \
+                  "--env TZ=${timezone}" \
+                  "--env VERSION=latest" \
+                  "--env PMM_CONFIG=/config/config.yml" \
+                  "--env PMM_RUN=True" \
+                  "--env PMM_TEST=False" \
+                  "--env PMM_NO_MISSING=False" \
+                  "--mount type=bind,source=/home/plexmeta,target=/config" \
+                  "--mount type=bind,source=/home/plexmeta,target=/home/plexmeta" \
+                  "lscr.io/linuxserver/plex-meta-manager:${architecture}-latest --run-once" \
+                  )
+else
+  service='metadata'
+  description='Plex Meta Manager'
+  timezone="$(/server/Properties.sh 'timezone')"
+  architecture="$(/server/Properties.sh 'architecture')"
+  startParameters=$(echo \
+                  "--env PUID=$(id -u plexmeta)" \
+                  "--env PGID=$(id -g plexmeta)" \
+                  "--env TZ=${timezone}" \
+                  "--env VERSION=latest" \
+                  "--env PMM_CONFIG=/config/config.yml" \
+                  "--env PMM_TIME=03:00" \
+                  "--env PMM_RUN=False" \
+                  "--env PMM_TEST=False" \
+                  "--env PMM_NO_MISSING=False" \
+                  "--mount type=bind,source=/home/plexmeta,target=/config" \
+                  "--mount type=bind,source=/home/plexmeta,target=/home/plexmeta" \
+                  "--restart unless-stopped lscr.io/linuxserver/plex-meta-manager:${architecture}-latest" \
+                  )
+fi
+
 
 /server/DockerService.sh "$0" "$service" "$description" "" "$startParameters" "$1"
 
